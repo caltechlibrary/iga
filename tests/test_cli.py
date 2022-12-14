c241ac314246ea9cbe89d347c2a33b343e1fcf6f
Michael Hucka <mhucka@caltech.edu>
2022-12-13 18:23:13 -0800

    Add tests for cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,10 +6,84 @@
 # @website https://github.com/caltechlibrary/iga
 # =============================================================================
 
-import plac
+import click
+import click.testing
+import os
+
+from iga.exit_codes import ExitCode
 
 
-def test_usage_help(capsys):
-    from iga.__main__ import main
-    with plac.Interpreter(main) as i:
-        i.check('-h', '')
+def test_no_args(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli)
+    assert result.exit_code == int(ExitCode.bad_arg)
+
+
+def test_unknown_arg(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['--foo'])
+    assert 'No such option' in result.output
+    assert result.exit_code == int(ExitCode.bad_arg)
+
+
+def test_help_flag(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['--help'])
+    assert 'Usage' in result.output
+    assert result.exit_code == int(ExitCode.success)
+    result = runner.invoke(cli, ['-h'])
+    assert 'Usage' in result.output
+    assert result.exit_code == int(ExitCode.success)
+
+
+def test_help_arg(monkeypatch):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['help'])
+    assert result.exit_code == int(ExitCode.success)
+
+
+def test_debug(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['--debug'])
+    assert 'requires an argument' in result.output
+    assert result.exit_code == int(ExitCode.bad_arg)
+
+
+def test_version(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['--version'])
+    assert 'version' in result.output
+    assert result.exit_code == int(ExitCode.success)
+
+
+def test_server(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['--server', 'foo', '--token', '-', 'fakerecord'])
+    assert result.exit_code == int(ExitCode.success)
+    assert 'RDM_SERVER' in os.environ
+    assert os.environ['RDM_SERVER'] == 'foo'
+
+
+def test_token(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    args = ['--server', 'foo', '--token', '-', 'fakerecord']
+    result = runner.invoke(cli, args, input='faketoken')
+    assert result.exit_code == int(ExitCode.success)
+    assert 'RDM_TOKEN' in os.environ
+    assert os.environ['RDM_TOKEN'] == 'faketoken'
+
+
+def test_incomplete_file_arg(capsys):
+    from iga.cli import cli
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ['-f'])
+    assert 'requires an argument' in result.output
+    assert result.exit_code == int(ExitCode.bad_arg)
