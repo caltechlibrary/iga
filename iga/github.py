005387a2e7bbbcbd753a09ac7c81b9b79e4c6417
Michael Hucka <mhucka@caltech.edu>
2022-12-16 19:03:12 -0800

    WIP simplified things a lot

--- iga/github.py
+++ iga/github.py
@@ -18,37 +18,29 @@ from iga.exit_codes import ExitCode
 from iga.exceptions import ReleaseError, GitHubError, InternalError
 
 
-class Release(SimpleNamespace):
+class GitHubRelease(SimpleNamespace):
+    '''Simple data structure corresponding to GitHub release JSON data.'''
     def __init__(self, release_dict):
         super().__init__(**release_dict)
-        self.author = SimpleNamespace(**release_dict['author'])
-        # First do in-place conversion of the uploader field (a dict), then do
-        # convert the dict of the containing asset.
+        self.author = GitHubAccount(**release_dict['author'])
+        # First, do in-place conversion of the 'uploader' field (a dict) ...
         for asset in self.assets:
-            asset['uploader'] = SimpleNamespace(**asset['uploader'])
-        self.assets = [SimpleNamespace(**asset) for asset in self.assets]
+            asset['uploader'] = GitHubAccount(**asset['uploader'])
+        # ... then convert the dict of the asset (which contains uploader).
+        self.assets = [GitHubAsset(**asset) for asset in self.assets]
 
 
-def well_formed(release_url):
-    return (release_url.startswith('https://github.com')
-            and '/releases/tag/' in release_url)
+class GitHubAccount(SimpleNamespace):
+    '''Simple data structure corresponding to GitHub account JSON objects.'''
 
 
-def owner_repo_tag(release_url):
-    # Example URL: https://github.com/mhucka/taupe/releases/tag/v1.2.0
-    _, _, owner, repo, _, _, tag = release_url.replace('/', ' ').split()
-    return (owner, repo, tag)
+class GitHubAsset(SimpleNamespace):
+    '''Simple data structure corresponding to GitHub file asset JSON objects.'''
 
 
 def github_release(release_url):
-    if not well_formed(release_url):
-        log('release_url not well-formed: ' + str(release_url))
-        raise ReleaseError('Malformed release URL: ' + str(release_url))
-
-    owner, repo, tag = owner_repo_tag(release_url)
-    url = 'https://api.github.com/repos/'+owner+'/'+repo+'/releases/tags/'+tag
-    (response, error) = net('get', url)
+    (response, error) = net('get', release_url)
     if error:
         log('error from GitHub: ' + str(error))
         raise GitHubError(str(error))
-    return Release(json.loads(response.text))
+    return GitHubRelease(json.loads(response.text))
