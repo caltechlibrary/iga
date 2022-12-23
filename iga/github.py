ee056aac773f6483e013162bd5704cc507a74b71
Michael Hucka <mhucka@caltech.edu>
2022-12-22 20:12:22 -0800

    Initial code for creating record

--- iga/github.py
+++ iga/github.py
@@ -9,7 +9,7 @@ file "LICENSE" for more information.
 '''
 
 from   commonpy.network_utils import net
-import json
+import json5
 from   sidetrack import log
 from   types import SimpleNamespace
 
@@ -41,9 +41,9 @@ class GitHubRepo(SimpleNamespace):
     def __init__(self, repo_dict):
         super().__init__(**repo_dict)
         self.owner = GitHubAccount(**repo_dict['owner'])
-        if 'organization' in repo_dict:
+        if repo_dict.get('organization', None):
             self.organization = GitHubAccount(**repo_dict['organization'])
-        if 'license' in repo_dict:
+        if repo_dict.get('license', None):
             self.license = GitHubLicense(**repo_dict['license'])
         # Save the original data for debugging purposes.
         self._json_dict = repo_dict
@@ -64,6 +64,10 @@ class GitHubLicense(SimpleNamespace):
 class GitHubFile(SimpleNamespace):
     '''Simple data structure corresponding to a file in a repo.'''
 
+
+class GitHubUser(SimpleNamespace):
+    '''Simple data structure corresponding to a user in GitHub.'''
+
 
 # Principal exported functions.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -74,6 +78,13 @@ def github_release(release_url):
     return object_for_github(release_url, GitHubRelease)
 
 
+def github_user(account):
+    '''Return a User object corresponding to the GitHub user account.'''
+    user_url = 'https://api.github.com/users/' + account
+    log('getting GitHub data for user at ' + user_url)
+    return object_for_github(user_url, GitHubUser)
+
+
 def github_repo(account, repo):
     '''Return a Repo object corresponding to the repo in GitHub.'''
     repo_url = 'https://api.github.com/repos/' + account + '/' + repo
@@ -81,17 +92,62 @@ def github_repo(account, repo):
     return object_for_github(repo_url, GitHubRepo)
 
 
-def github_repo_files(repo):
+def github_repo_filenames(repo):
     '''Return a list of file information objects for the given repo.'''
+    if getattr(repo, '_filenames', None):
+        return repo._filenames
     log('asking GitHub for list of files at ' + repo.api_url)
     files_url = repo.api_url + '/git/trees/' + repo.default_branch
     (response, error) = net('get', files_url)
     if error:
         log('unable to get listof files for repo: ' + str(error))
         return []
-    json_dict = json.loads(response.text)
-    log(f'found {len(json_dict["tree"])} files in repo')
-    return [GitHubFile(**f) for f in json_dict['tree']]
+    json_dict = json5.loads(response.text)
+    files = [GitHubFile(**f) for f in json_dict['tree']]
+    log(f'found {len(files)} files in repo')
+    # Cache the results on the repo object, so we don't have to recompute it.
+    repo._files = files
+    repo._filenames = [file.path for file in files]
+    return repo._filenames
+
+
+def github_repo_file(repo, filename):
+    '''Return the text contents of the named file in the repo.'''
+    if filename not in github_repo_filenames(repo):
+        return ''
+    if filename in getattr(repo, '_files_contents', {}):
+        return repo._files_contents[filename]
+    file = next(f for f in repo._files if f.path == filename)
+    (response, error) = net('get', file.url)
+    if error:
+        return ''
+    file_dict = json5.loads(response.text)
+    if file_dict['encoding'] != 'base64':
+        log(f'GitHub file encoding for {filename} is ' + file_dict['encoding'])
+        raise InternalError('Unimplemented file encoding ' + file_dict['encoding'])
+    import base64
+    contents = base64.b64decode(file_dict['content']).decode()
+    if not getattr(repo, '_file_contents', {}):
+        repo._file_contents = {}
+    # Cache the file contents, so we don't have to get it from GitHub again.
+    repo._file_contents[filename] = contents
+    return contents
+ 
+
+def github_account_repo_tag(release_web_url):
+    '''Return tuple (account, repo name, tag) based on the given web URL.'''
+    # Example URL: https://github.com/mhucka/taupe/releases/tag/v1.2.0
+    # Note this is not the same as the "release url" below.
+    _, _, _, account, repo, _, _, tag = release_web_url.split('/')
+    return (account, repo, tag)
+
+
+def github_release_url(account, repo, tag):
+    return 'https://api.github.com/repos/'+account+'/'+repo+'/releases/tags/'+tag
+
+
+def github_file_url(repo, filename):
+    return repo.html_url + '/blob/' + repo.default_branch + '/' + filename
 
 
 def valid_github_release_url(release_web_url):
@@ -102,13 +158,6 @@ def valid_github_release_url(release_web_url):
             and split_url[5] == 'releases'
             and split_url[6] == 'tag')
 
-
-def github_account_repo_tag(release_web_url):
-    '''Return tuple (account, repo name, tag) based on the given release URL.'''
-    # Example URL: https://github.com/mhucka/taupe/releases/tag/v1.2.0
-    _, _, _, account, repo, _, _, tag = release_web_url.split('/')
-    return (account, repo, tag)
-
 
 # Helper functions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -116,7 +165,7 @@ def github_account_repo_tag(release_web_url):
 def object_for_github(api_url, cls):
     (response, error) = net('get', api_url)
     if error:
-        import commonpy
+        import commonpy.exceptions
         if isinstance(error, commonpy.exceptions.AuthenticationFailure):
             # GitHub returns 403 when you hit the rate limit.
             # https://docs.github.com/en/rest/overview/resources-in-the-rest-api
@@ -133,10 +182,11 @@ def object_for_github(api_url, cls):
     log('unpacking JSON into object structure')
     try:
         # Create the desired object & add the api url in case it's needed later.
-        obj = cls(json.loads(response.text))
+        obj = cls(json5.loads(response.text))
         obj.api_url = api_url
         return obj
-    except json.decoder.JSONDecodeError as ex:
+    except Exception as ex:
+        breakpoint()
         # GitHub returned an unexpected value. We need to fix our handling.
         log('unexpected problem decode json from GitHub: ' + str(ex))
         raise InternalError('GitHub returned an unexpected value.')
--- iga/record.py
+++ iga/record.py
@@ -3,6 +3,33 @@ record.py: record structure creation & manipulation
 
 This file is part of https://github.com/caltechlibrary/iga/.
 
+The code in this file constructs a metadata record in the format expected by
+InvenioRDM. It uses data provided in a GitHub release as well as the repository
+(and files in the repository), but because GitHub releases and repos don't
+directly contain the information needed, we have to resort to looking for and
+parsing files in the repo to try to extract the info we want. The most useful
+such files are codemeta.json and CITTATION.cff, but we resort to other things
+if we can't find them.
+
+CITATION.cff and codemeta.json overlap in their contents, so a natural question
+is which one to try to use first. Stephan Druskat wrote the following about
+them in https://github.com/citation-file-format/cff-converter-python/issues/4:
+
+* "CodeMeta aims to, generally, provide a minimal schema for metadata
+  for research software. It isn't necessarily tailored for citation metadata
+  but can be used to provide metadata that can be used for citation."
+
+* "CFF aims to provide a format for the explicit and exclusive provision of
+  citation metadata for research software. Things like transformability to
+  BibTeX and RIS have been taken into account. As such, CFF is both less
+  comprehensive in terms of general metadata (although I will extend it to
+  cover the whole key set of CodeMeta at some point), and more "detailed" in
+  terms of citation metadata."
+
+Since the use of InvenioRDM is more about archiving repository code than about
+citing software, the code below looks for and uses codemeta.json first,
+followed by CITATION.cff if a codemeta file can't be found.
+
 Copyright (c) 2022 by the California Institute of Technology.  This code
 is open-source software released under a BSD-type license.  Please see the
 file "LICENSE" for more information.
@@ -10,172 +37,334 @@ file "LICENSE" for more information.
 
 import arrow
 import copy
-from   sidetrack import log
+import json5
 from   os import path
+from   sidetrack import log
+import sys
 
 from iga.exceptions import InternalError
-from iga.github import github_release, github_repo, github_repo_files
+from iga.github import (
+    github_release,
+    github_release_url,
+    github_repo,
+    github_repo_file,
+    github_repo_filenames,
+    github_file_url
+)
 
 
-# Internal constants.
+# Constants.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-_SCHEMA_PATH = path.join(path.dirname(__file__), 'data/datacite_4.3_schema.json')
+# InvenioRDM schema info: https://inveniordm.docs.cern.ch/reference/metadata/.
+# A record can have these top-level fields (but might not contain all):
+#
+# {
+#    "$schema": "local://records/record-vX.Y.Z.json",
+#    "id": "q5jr8-hny72",
+#    "pid": { ... },
+#    "pids" : { ... },
+#    "parent": { ... },
+#    "access" : { ... },
+#    "metadata" : { ... },
+#    "files" : { ... },
+#    "tombstone" : { ... },
+#    "created": "...",
+#    "updated": "...",
+# }
+#
+# The minimum for a record sent to InvenioRDM is the "metadata" field. The
+# following is the full set of subfields in "metadata".
+
+FIELDS = [
+    "additional_descriptions",
+    "additional_titles",
+    "contributors",
+    "creators",
+    "dates",
+    "description",
+    "formats",
+    "funding",
+    "identifiers",
+    "languages",
+    "locations",
+    "publication_date",
+    "publisher",
+    "references",
+    "related_identifiers",
+    "resource_type",
+    "rights",
+    "sizes",
+    "subjects",
+    "title",
+    "version",
+]
+
+# A number of these fields are created by InvenioRDM itself; a user doesn't
+# put them in a record sent to InvenioRDM.  Based on the test cases in the repo
+# https://github.com/inveniosoftware/invenio-rdm-records, the min fields are:
+#
+# {
+#    "metadata": {
+#        "resource_type": { "id": "XYZ", ... },          # note below
+#        "title": "ABC",
+#        "creators": [
+#              {
+#                  "person_or_org": {
+#                      "family_name": "A",
+#                      "given_name": "B",
+#                      "type": "C",
+#                  }
+#              },
+#            ],
+#        "publication_date": "...date...",
+#    }
+
+REQUIRED_FIELDS = [
+    "creators",
+    "publication_date",
+    "resource_type",
+    "title"
+]
 
 
-# Principal exported functions.
+# Exported module functions.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def valid_record(record):
-    '''Return True if the given record is valid.'''
-    import json
-    import jsonschema
+def valid_record(json_dict):
+    '''Perform basic validation on the given JSON record for InvenioRDM.'''
 
-    # Step 1: do basic JSON validation.
-    try:
-        record_json = json.loads(record)
-    except json.decoder.JSONDecodeError as ex:
-        log('given record failed to load using Python json library: ' + str(ex))
+    # The validation process currently on tests that the record has the
+    # minimum fields; it does not check the field values.
+    if 'metadata' not in json_dict:
+        log('record lacks a "metadata" field')
         return False
-
-    # Step 2: validate against DataCite JSON schema.
-    try:
-        with open(_SCHEMA_PATH, 'r') as schema_file:
-            datacite_schema = schema_file.read()
-        schema_json = json.loads(datacite_schema)
-        jsonschema.validate(instance=record_json, schema=schema_json)
-    except OSError as ex:
-        raise InternalError('Unable to read internal JSON schema: ' + str(ex))
-    except json.decoder.JSONDecodeError as ex:
-        raise InternalError('Internal JSON schema is corrupted: ' + str(ex))
-    except jsonschema.exceptions.ValidationError as ex:
-        log('given record failed schema validation: ' + str(ex))
-        return False
+    metadata = json_dict['metadata']
+    # Yes, this could be done by a simple 1-liner, but we want to log failures.
+    for field in REQUIRED_FIELDS:
+        if field not in metadata:
+            log('record metadata lacks required field "' + field + '"')
+            return False
+    log('record metadata has the required minimum set of fields')
     return True
 
 
 def record_from_release(account, repo, tag):
+    '''Return InvenioRDM record created from the account/repo/tag release.'''
     release_url = github_release_url(account, repo, tag)
     release = github_release(release_url)
     repo = github_repo(account, repo)
-    record = copy.deepcopy(_RECORD_TEMPLATE)
-    for key, value in record.items():
-        if callable(value):
-            record[key] = value(repo, release)
-    breakpoint()
-    return record
 
+    # We use codemeta.json & CITATION.cff often. Get them now & add as property.
+    repo.codemeta = {}
+    repo.cff = {}
+    filenames = github_repo_filenames(repo)
+    if 'codemeta.json' in filenames:
+        repo.codemeta = json5.loads(github_repo_file(repo, 'codemeta.json'))
+    cff_filename = None
+    if 'CITATION.cff' in filenames:
+        cff_filename = 'CITATION.cff'
+    elif 'CITATION.CFF' in filenames:
+        cff_filename = 'CITATION.CFF'
+    if cff_filename:
+        from cffconvert.citation import Citation
+        file_url = github_file_url(repo, cff_filename)
+        contents = github_repo_file(repo, cff_filename)
+        cff_object = Citation(contents, src=file_url)
+        repo.cff = json5.loads(cff_object.as_schemaorg())
 
-def github_release_url(account, repo, tag):
-    return 'https://api.github.com/repos/'+account+'/'+repo+'/releases/tags/'+tag
+    # Make the metadata dict by iterating over the names in FIELDS and calling
+    # the function of that name defined in this (module) file.
+    module = sys.modules[__name__]
+    metadata = dict((f, getattr(module, f)(repo, release)) for f in FIELDS)
+    return {"metadata": metadata}
 
 
-# Helper functions.
+# Field value functions.
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Summary of the approach: the functions for extracting values from GitHub
+# are named after the fields themselves, so that record_from_release(...)
+# above can create a record by simply iterating over the names in FIELDS and
+# calling the function of that name to get the value for that field.
 
-def type_extractor(repo, release):
-    # FIXME: handle repos that are not software
-    return {"resourceTypeGeneral": "Software", "resourceType": "Software"}
+def additional_descriptions(repo, release):
+    descriptions = []
+    # The codemeta and CFF descriptions tend to be better and longer than
+    # what people write in GitHub repo descriptions, so use that if possible.
+    # It doesn't seem useful to put both one of these *and* the GitHub repo
+    # description; that's why the conditional below is an if-else.
+    text = repo.codemeta.get('description', None) or repo.cff.get('description', None)
+    if text:
+        descriptions.append({'description': text,
+                             'type': {'id': 'other', 'title': {'en': 'Other'}}})
+    elif repo.description:
+        descriptions.append({'description': repo.description,
+                             'type': {'id': 'other', 'title': {'en': 'Other'}}})
+    return descriptions
 
 
-def titles_extractor(repo, release):
-    return [{'title': repo.name + ': ' + release.name}]
+def additional_titles(repo, release):
+    return []
 
 
-def descriptions_extractor(repo, release):
-    return [{'descriptions': release.body}]
+def contributors(repo, release):
+    if repo.codemeta.get('contributor', []):
+        return [_person(x) for x in repo.codemeta.get('contributor', [])]
+    if repo.cff.get('contributors', []):
+        return [_person(x) for x in repo.cff.get('contributors', [])]
+    return []
 
 
-def version_extractor(repo, release):
+def creators(repo, release):
+    # Codemeta & CITATION.cff files contain more complete author info than the
+    # GitHub release data, so try them 1st.
+    if repo.codemeta:
+        return [_person(x) for x in repo.codemeta.get('author', [])]
+    elif repo.cff:
+        return [_person(x) for x in repo.cff.get('author', [])]
+    else:
+        # We can call GitHub's user data API, but it returns very little info
+        # about a user (e.g.,, it gives a name but that name is not broken out
+        # into family & given name.
+        breakpoint()
+
+
+def dates(repo, release):
+    created_date = arrow.get(release.created_at)
+    return [{'date': created_date.format('YYYY-MM-DD'),
+             'type': {'id': 'created', 'title': {'en': 'Created'}}}]
+
+
+def description(repo, release):
+    return release.body.strip()
+
+
+def formats(repo, release):
+    # fixme
+    return []
+
+
+def funding(repo, release):
+    return []
+
+
+def identifiers(repo, release):
+    return []
+
+
+def languages(repo, release):
+    # GitHub doesn't provide a way to deal with any other human language.
+    return [{"id": "eng"}]
+
+
+def locations(repo, release):
+    return []
+
+
+def publication_date(repo, release):
+    return arrow.get(release.published_at).format('YYYY-MM-DD')
+
+
+def publisher(repo, release):
+    return 'CaltechDATA'
+
+
+def references(repo, release):
+    # FIXME some people put paper citation info in CITATION.cff
+    # See eg https://github.com/plasma-umass/scalene
+    return []
+
+
+def related_identifiers(repo, release):
+    return [{'identifier': release.html_url,
+             'relation_type': {'id': 'isidenticalto',
+                               'title': {'en': 'Is identical to'}},
+             'scheme': 'url'}]
+
+
+def resource_type(repo, release):
+    # InvenioRDM requires that the resource_type id comes from a CV. We only
+    # recognize specific types, so we hardwire the values here.
+
+    # FIXME handle other types like datasets
+    return {'id': 'software', 'title': {'en': 'Software'}}
+
+
+def rights(repo, release):
+    if repo.license and repo.license.name != 'Other':
+        spdx_id = repo.license.spdx_id
+        rights = [{'id': spdx_id,
+                   'link': repo.license.url,
+                   'title': {'en': repo.license.name}}]
+        from iga.licenses import LICENSE_DESCRIPTIONS
+        if spdx_id in LICENSE_DESCRIPTIONS:
+            rights[0].update({'description': {'en': LICENSE_DESCRIPTIONS[spdx_id]}})
+        return rights
+
+    # GitHub didn't fill in the license info -- maybe it didn't recognize
+    # the license or its format. Try to look for a license file ourselves.
+    filenames = github_repo_filenames(repo)
+    base_url = 'https://github.com/' + repo.owner.login + '/' + repo.name
+    # The FSF guide on how to use the GPL suggests COPYING. The Producing
+    # Open Source Software book suggests either COPYING or LICENSE.
+    for basename in ['LICENSE', 'License', 'license', 'COPYING',
+                     'COPYRIGHT', 'Copyright', 'copyright']:
+        for ext in ['', '.txt', '.md', '.html']:
+            if basename + ext in filenames:
+                # There's no safe way to summarize arbitrary license
+                # text, so we can't provide 'description' field value.
+                return [{'title': {'en': 'License'},
+                         'link': base_url + '/' + basename + ext}]
+
+    # We didn't find a license file. Try codemeta.json & CITATION.cff.
+    value = repo.codemeta.get('license', None) or repo.cff.get('license', None)
+    if value:
+        if 'http' in value:
+            return [{'title': {'en': 'License'},
+                     'link': value}]
+        else:
+            # Not a URL. Assume it's a license name.
+            return [{'title': {'en': value}}],
+
+    # We didn't find anything usable.
+    return []
+
+
+def sizes(repo, release):
+    # fixme
+    return []
+
+
+def subjects(repo, release):
+    return [{'subject': x} for x in repo.topics if x.lower() != 'github']
+
+
+def title(repo, release):
+    return repo.full_name + ': ' + release.name
+
+
+def version(repo, release):
     return release.tag_name
 
+
+# Miscellaneous helper functions.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def dates_extractor(repo, release):
-    created_date = arrow.get(release.created_at).format('YYYY-MM-DD')
-    issued_date = arrow.get(release.published_at).format('YYYY-MM-DD')
-    return [
-        {'dateType': 'Created', 'date': created_date},
-        {'dateType': 'Issued', 'date': issued_date},
-    ]
-
-
-def creators_extractor(repo, release):
-    return []
-
-
-def contributors_extractor(repo, release):
-    return []
-
-
-def pubyear_extractor(repo, release):
-    date = arrow.get(release.published_at)
-    return str(date.year)
-
-
-def subjects_extractor(repo, release):
-    return [{'subject': x} for x in repo.topics] if repo.topics else []
-
-
-def identifiers_extractor(repo, release):
-    return []
-
-
-def related_identifiers_extractor(repo, release):
-    return []
-
-
-def sizes_extractor(repo, release):
-    return ''
-
-
-def formats_extractor(repo, release):
-    return ''
-
-
-def rightslist_extractor(repo, release):
-    if repo.license.name != 'Other':
-        return [{'rights': 'license',
-                 'rightsUri': repo.license.url,
-                 'rightsIdentifierScheme': 'SPDX',
-                 'rightsIdentifier': repo.license.spdx_id,
-                 'schemeUri': 'https://spdx.org/licenses/'}]
-    else:
-        # GitHub didn't fill in the license field because it didn't recognize
-        # what's there. Try to find a recognizable license file anyway.
-        files = github_repo_files(repo)
-        names = [file.path for file in files]
-        base_url = 'https://github.com/' + repo.owner.login + '/' + repo.name
-        for ext in ['', '.txt', '.md', '.html']:
-            for basename in ['LICENSE', 'license', 'License']:
-                if basename + ext in names:
-                    return [{'rights': 'license',
-                             'rightsUri': base_url + '/' + basename + ext}]
+def _person(person_dict):
+    # See https://inveniordm.docs.cern.ch/reference/metadata/#creators-1-n
+    # Although people sometimes put more things in the CITATION.cff authors
+    # info (e.g., email addr), there's no provision in InvenioRDM for that.
+    person = {'family_name': person_dict['familyName'],
+              'given_name': person_dict['givenName'],
+              'type': 'personal'}
+    if 'orcid' in person_dict.get('@id', ''):
+        orcid = person_dict['@id'].split('/')[-1]
+        person.update({'identifiers': [{'scheme': 'orcid',
+                                        'identifier': orcid}]})
+    if 'affiliation' in person_dict:
+        if isinstance(person_dict['affiliation'], dict):
+            affiliation = person_dict['affiliation']['legalName']
         else:
-            return []
-
-
-def funding_extractor(repo, release):
-    return []
-
-
-_RECORD_TEMPLATE = {
-    "schemaVersion"      : "http://datacite.org/schema/kernel-4",
-    "types"              : type_extractor,
-    "titles"             : titles_extractor,
-    "descriptions"       : descriptions_extractor,
-    "version"            : version_extractor,
-    "dates"              : dates_extractor,
-    "creators"           : creators_extractor,
-    "contributors"       : contributors_extractor,
-    "publisher"          : "CaltechDATA",
-    "publicationYear"    : pubyear_extractor,
-    "subjects"           : subjects_extractor,
-    "identifiers"        : identifiers_extractor,
-    "relatedIdentifiers" : related_identifiers_extractor,
-    "sizes"              : sizes_extractor,
-    "formats"            : formats_extractor,
-    "rightsList"         : rightslist_extractor,
-    "fundingReferences"  : funding_extractor,
-    "geoLocations"       : [],
-    "language"           : "en",
-}
+            affiliation = person_dict['affiliation']
+        person.update({'affiliations': [{'name': affiliation}]})
+    return person
