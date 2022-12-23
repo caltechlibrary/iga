094ee85c01b6703a997127e3b4af0ff23f4e1fbe
Michael Hucka <mhucka@caltech.edu>
2022-12-22 20:10:47 -0800

    Add start at invenio.py

---
+++ iga/invenio.py
@@ -0,0 +1,12 @@
+'''
+invenio.py: Code for interacting with InvenioRDM
+
+This file is part of https://github.com/caltechlibrary/iga/.
+
+Copyright (c) 2022 by the California Institute of Technology.  This code
+is open-source software released under a BSD-type license.  Please see the
+file "LICENSE" for more information.
+'''
+
+def invenio_write(record, server, token):
+    breakpoint()
