cfb2c9c149030f948a782e68eb366c670d749402
Michael Hucka <mhucka@caltech.edu>
2023-01-12 19:24:32 -0800

    Fix for change in success exit code message

--- tests/test_exit_codes.py
+++ tests/test_exit_codes.py
@@ -3,4 +3,4 @@ from iga.exit_codes import ExitCode
 
 def test_exitcode():
     assert int(ExitCode.success) == 0
-    assert ExitCode.success.meaning == "success -- program completed normally"
+    assert ExitCode.success.meaning == "success – program completed normally"
