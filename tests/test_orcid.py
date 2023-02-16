c5aa66a497fa814f8ca8805663ac37ce8cf9f6ef
Michael Hucka <mhucka@caltech.edu>
2023-02-16 09:13:15 -0800

    Test the ORCID value of a deleted entry

--- tests/test_orcid.py
+++ tests/test_orcid.py
@@ -6,3 +6,4 @@ def test_name_from_orcid():
     assert name_from_orcid('0000-0001-9105-5960') == ('Michael', 'Hucka')
     assert name_from_orcid('0000-0002-2746-5647') == ('Jessica A.', 'Lavery')
     assert name_from_orcid('https://orcid.org/0000-0003-0900-6903') == ('R. S.', 'Doiel')
+    assert name_from_orcid('https://orcid.org/0000-0001-6151-2200') == ('', '')
