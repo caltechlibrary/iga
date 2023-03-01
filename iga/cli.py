8da2705419235ecfd42aea8de924b468dcc6ab43
Michael Hucka <mhucka@caltech.edu>
2023-02-28 20:13:27 -0800

    Change handling of tokens
    Using file inputs for the token is not only annoying but also unlikely
    to be helpful, because when running as a GitHub action, we'll want to
    use environment variables anyway. For testing, it's convenient to pass
    the tokens on the command line, so that's what it now does.
    
    Also changed the names of some of the options for greater clarity.

--- iga/cli.py
+++ iga/cli.py
@@ -51,37 +51,51 @@ def _config_debug(ctx, param, debug_dest):
     return debug_dest
 
 
-def _read_token(ctx, param, file_):
-    '''Read the file and set the environment variable RDM_TOKEN.'''
-    if ctx.params.get('url_or_tag', None) == 'help':
-        print_help_and_exit(ctx)
-    elif file_:
-        log('reading token from file')
-        os.environ['RDM_TOKEN'] = file_.read()
-    elif 'RDM_TOKEN' in os.environ:
-        log('RDM_TOKEN found in environment')
-    else:
-        alert(ctx, 'Cannot proceed without a Personal Access Token. (Tip: provide'
-              ' the `--token` option or set the environment variable **RDM_TOKEN**.)')
-        sys.exit(int(ExitCode.bad_arg))
-    return os.environ['RDM_TOKEN']
+def _read_param_value(ctx, param, value, env_var, purpose):
+    '''Handle reading a CLI argument.
 
+    If a value is passed on the command line, the environment variable
+    env_var is set to the value, and the value is returned.
 
-def _read_server(ctx, param, value):
-    '''Read the server address and set the environment variable RDM_SERVER.'''
+    If CLI argument value is None, the environment variable env_var is checked
+    and if it has a value, it's returned; otherwise, this function prints an
+    error message and exits with a status code signifying "bad argument".
+
+    If the value is "help", this function prints help text and causes the
+    program to exit.
+    '''
     if ctx.params.get('url_or_tag', None) == 'help':
         print_help_and_exit(ctx)
     elif value:
-        log('server address given on command line: ' + value)
-        os.environ['RDM_SERVER'] = value
-    elif 'RDM_SERVER' in os.environ:
-        log('RDM_SERVER found in environment')
+        log(f'using {value} for the {purpose} passed on the command line')
+        os.environ[env_var] = value
+    elif env_var in os.environ:
+        log(f'env var {env_var} is set; using it as the value for the {purpose}')
     else:
-        alert(ctx, 'Cannot proceed without an InvenioRDM server address. (Tip:'
-              ' provide the `--server` option or set the environment variable'
-              ' **RDM_SERVER**.)')
+        opt = param.name.replace('_', '-')
+        alert(ctx, f'Cannot proceed without a value for the {purpose}. (Tip:'
+              f' use the `--{opt}` option or set the variable **{env_var}**.'
+              ' For more information, use the option `--help`.)')
         sys.exit(int(ExitCode.bad_arg))
-    return os.environ['RDM_SERVER']
+    return os.environ[env_var]
+
+
+def _read_github_token(ctx, param, value):
+    '''Read the file and set the environment variable GITHUB_TOKEN.'''
+    return _read_param_value(ctx, param, value, 'GITHUB_TOKEN',
+                             'GitHub personal access token')
+
+
+def _read_invenio_token(ctx, param, value):
+    '''Read the file and set the environment variable INVENIO_TOKEN.'''
+    return _read_param_value(ctx, param, value, 'INVENIO_TOKEN',
+                             'InvenioRDM personal access token')
+
+
+def _read_server(ctx, param, value):
+    '''Read the server address and set the environment variable INVENIO_SERVER.'''
+    return _read_param_value(ctx, param, value, 'INVENIO_SERVER',
+                             'InvenioRDM server address')
 
 
 def _print_version_and_exit(ctx, param, value):
@@ -106,10 +120,7 @@ def _print_version_and_exit(ctx, param, value):
 @click.command(add_help_option=False)
 @click.help_option('--help', '-h', help='Show this message and exit')
 #
-@click.option('--account', '-a', metavar='TEXT',
-              help='GitHub account name (if not using a release URL)')
-#
-@click.option('--community', '-c', metavar='ID',
+@click.option('--community', '-c', metavar='STR',
               help='ID of RDM community to update with item')
 #
 @click.option('--dry-run', '-n', is_flag=True,
@@ -118,48 +129,60 @@ def _print_version_and_exit(ctx, param, value):
 @click.option('--file', '-f', 'given_files', metavar='FILE', multiple=True,
               help='File to upload (repeat for multiple files)')
 #
-@click.option('--record', '-r', 'given_record', metavar='FILE', type=File('r'),
-              help='Record to use for the entry in InvenioRDM')
+@click.option('--github-account', '-a', metavar='STR',
+              help='GitHub account name (if not using a release URL)')
 #
-@click.option('--repo', '-p', metavar='TEXT',
+@click.option('--github-repo', '-r', metavar='STR',
               help='GitHub repository name (if not using a release URL)')
 #
-@click.option('--server', '-s', metavar='URL', callback=_read_server,
-              help='Address of InvenioRDM server')
+@click.option('--github-token', '-g', metavar='STR', callback=_read_github_token,
+              help="GitHub acccess token (avoid – use variable)")
 #
-@click.option('--token', '-t', metavar='FILE', type=File('r'), callback=_read_token,
-              help="File ('-' for stdin) containing a PAT for InvenioRDM")
+@click.option('--invenio-server', '-s', metavar='URL', callback=_read_server,
+              help='InvenioRDM server address')
+#
+@click.option('--invenio-token', '-t', metavar='STR', callback=_read_invenio_token,
+              help="InvenioRDM access token (avoid – use variable)")
+#
+@click.option('--record', '-R', 'given_record', metavar='FILE', type=File('r'),
+              help='Metadata record to use for the InvenioRDM entry')
 #
 @click.option('--version', '-V', callback=_print_version_and_exit, is_flag=True,
-              help='Print version info and exit', expose_value=False)
+              help='Print version info and exit', expose_value=False, is_eager=True)
 #
-@click.option('--debug'  , '-@', metavar='OUT', type=File('w', lazy=False),
-              callback=_config_debug, help='Write debug output to destination "OUT"')
+@click.option('--debug'  , '-@', metavar='DEST', type=File('w', lazy=False),
+              callback=_config_debug, help='Write debug output to destination "DEST"')
 #
 @click.argument('url_or_tag', required=True)
 @click.pass_context
 def cli(ctx, url_or_tag, account=None, community=None, dry_run=False,
         given_files=None, given_record=None, repo=None, server=None,
-        token=None, help=False, version=False, debug=False):  # noqa A002
+        github_token=None, rdm_token=None,
+        help=False, version=False, debug=False):  # noqa A002
     '''InvenioRDM GitHub Archiver (IGA) command-line interface.
 \r
 IGA retrieves a GitHub software release and archives it in an InvenioRDM
-server. The release can be specified using _either_ a full URL, _or_ an
+server. The release can be specified using _either_ a full URL, _or_ a GitHub
 account + repository + tag combination (see below). By default, IGA constructs
 a record based on information extracted from the GitHub repository; this can be
 overriden by command-line options described below.
 \r
-_**InvenioRDM server and access token**_
+_**Specification of the InvenioRDM server and access token**_
 \r
 The server address must be provided either as the value of the option
-`--server` _or_ in an environment variable named `RDM_SERVER`.
+`--invenio-server` _or_ in an environment variable named `INVENIO_SERVER`.
 \r
 A Personal Access Token (PAT) for making API calls to the InvenioRDM server
-must be also supplied when invoking IGA, either in a file whose path is given
-as the value of the option `--token` (use `-` for standard input), _or_ in an
-environment variable named `RDM_TOKEN`. (To obtain a PAT from an InvenioRDM
-server, visit the page `/account/settings/applications` on the server after
-logging in to your account and use the interface there to create a token.)
+must be also supplied when invoking IGA. The preferred way of doing that is to
+set the value of the environment variable `INVENIO_TOKEN`. Alternatively, you
+can use the option `--invenio-token` to pass the token on the command line, but
+**you are strongly advised to avoid this practice because it is insecure**.
+The option is provided for convenience during testing but should not be used
+in production.
+\r
+To obtain a PAT from an InvenioRDM server, first log in to the server, then
+visit the page at `/account/settings/applications` and use the interface there
+to create a token.
 \r
 _**Specification of a GitHub release**_
 \r
@@ -171,16 +194,34 @@ A GitHub release can be specified to IGA in one of two mutually-exclusive ways:
     case, the final argument on the command line must be the _tag_, and in
     addition, values for the options `--account` and `--repo` must be provided.
 \r
-Here's an example using approach #1:
+Here's an example using approach #1 (assuming environment variables
+INVENIO_SERVER, INVENIO_TOKEN, and GITHUB_TOKEN have all been set):
 ```
-iga --server https://data.caltech.edu --token token.txt \\
-    https://github.com/mhucka/taupe/releases/tag/v1.2.0
+    iga https://github.com/mhucka/taupe/releases/tag/v1.2.0
 ```
 and here's the equivalent using approach #2:
 ```
-iga --server https://data.caltech.edu --token token.txt \\
-    --account mhucka --repo taupe v1.2.0
+    iga --github-account mhucka --github-repo taupe v1.2.0
 ```
+\r
+_**Use of a GitHub access token**_
+\r
+It is possible to run IGA without providing a GitHub access token. GitHub
+allows up to 60 API calls per minute when running without credentials, and
+though IGA makes several API calls to GitHub each time it runs, for many
+repositories, IGA will not hit the limit. However, if you run IGA multiple
+times in a row or your repository has many contributors (IGA has to ask about
+each contributor individually), then you may need to supply a GitHub access
+token. The preferred way of doing that is to set the value of the environment
+variable `GITHUB_TOKEN`. Alternatively, you can use the option
+`--github-token` to pass the token on the command line, but **you are
+strongly advised to avoid this practice because it is insecure**.  The option
+is provided for convenience during testing but should not be used in
+production.
+\r
+To obtain a PAT from GitHub, visit https://docs.github.com/en/authentication
+and look for instructions about creating a "classic" personal access token.
+\r
 _**Construction of an InvenioRDM record**_
 \r
 The record created in InvenioRDM is constructed using information extracted
@@ -214,10 +255,14 @@ _**Other options recognized by IGA**_
 Running IGA with the option `--help` will make it print help text and exit
 without doing anything else.
 \r
+Running IGA with the option `--dry-run` will make it do the work of creating
+a metadata record, but instead of creating an archive in the InvenioRDM server,
+it will only print the record to the terminal.
+\r
 If given the `--version` option, this program will print its version and other
 information, and exit without doing anything else.
 \r
-If given the `--debug` argument, taupe will output details about what it is
+If given the `--debug` argument, IGA will output details about what it is
 doing. The debug trace will be sent to the given destination, which can be `-`
 to indicate console output, or a file path to send the debug output to a file.
 \r
@@ -323,7 +368,7 @@ def report_actions(ctx, record):
     from rich import print_json
 
     comment = (f'Option `--dry-run` is in effect. The following is the record'
-               f' that _would_ have been sent to **{os.environ["RDM_SERVER"]}**'
+               f' that _would_ have been sent to **{os.environ["INVENIO_SERVER"]}**'
                f' if `--dry-run` were _not_ in effect.')
     console = Console()
     console.print(
