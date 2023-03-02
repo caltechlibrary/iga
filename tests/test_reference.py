1a7217bb038fb259117593c6f90680dd3b3a8738
Michael Hucka <mhucka@caltech.edu>
2023-03-01 19:11:40 -0800

    Rename test_reference_utils to test_reference

--- tests/test_reference.py
+++ tests/test_reference.py
@@ -1,13 +1,7 @@
-from iga.reference_utils import *
+from iga.reference import *
 from sidetrack import set_debug
 
-
 set_debug(True, '/tmp/debug.log')
-
-
-def test_clean_text():
-    assert clean_text('foo <i>bar</i>') == 'foo bar'
-    assert clean_text('Sjoberg, D., D., Whiting, K., Curry, M., Lavery, J., A., & Larmarange, J. (2021). Reproducible Summary Tables with the gtsummary Package. The R Journal, 13(1), 570. https://doi.org/10.32614/rj-2021-053\n') == 'Sjoberg, D., D., Whiting, K., Curry, M., Lavery, J., A., & Larmarange, J. (2021). Reproducible Summary Tables with the gtsummary Package. The R Journal, 13(1), 570. https://doi.org/10.32614/rj-2021-053'
 
 
 def test_doi_from_pubmed():
