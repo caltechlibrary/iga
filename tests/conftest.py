fe9ce400144dbccf4068e64ac53fce7486a57d6f
Michael Hucka <mhucka@caltech.edu>
2023-04-06 18:06:23 -0700

    chore: always turn on debug mode in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,25 @@
+import os
 from   os.path import dirname, abspath, splitext, join, basename
 import pytest
 from   sidetrack import set_debug
 
 
-@pytest.fixture(scope="module", autouse=True)
-def saver_debug_log(request):
-    '''Turn on debug logging & save the output to "testfile.log".'''
+@pytest.fixture(scope='module', autouse=True)
+def save_debug_log(request):
+    '''Turn on debug logging & save the output to 'testfile.log'.'''
     here = dirname(abspath(__file__))
     mod_name = request.module.__name__
     log_file = join(here, splitext(basename(mod_name))[0] + '.log')
     open(log_file, 'w').close()
     set_debug(True, log_file)
+    os.environ['IGA_RUN_MODE'] = 'debug'
     yield
+
+
+# @pytest.fixture(scope='function')
+# def unset_environment(request, monkeypatch):
+#     '''Set MY_VARIABLE environment variable, this fixture must be used with `parametrize`'''
+#     monkeypatch.delenv('INVENIO_SERVER')
+#     monkeypatch.delenv('INVENIO_TOKEN')
+#     monkeypatch.delenv('GITHUB_TOKEN')
+#     yield request.param
